ea96c60a2b94bb456db5b7b2855e220c5bbceeb7
austinsamsel <austinsamsel@gmail.com>
2015-02-12 23:54:07 -0500

    tests passing

--- spec/controllers/works_controller_spec.rb
+++ spec/controllers/works_controller_spec.rb
@@ -179,6 +179,7 @@ describe WorksController do
     before :each do
       @client = create(:client) 
       @work = create(:work, client: @client) 
+      @users_work = create(:work)
       login_with create( :alt )
     end
 
@@ -191,93 +192,63 @@ describe WorksController do
         #   get :index
         #   expect(assigns(:works)).to match_array([smith])
         # end
-        it "renders the :index template" do
+        it "redirects when... renders the :index template" do
           get :index, client_id: @client.id
-          expect(response).to render_template :index
+          expect(response).to redirect_to root_url
         end
       end
     end
 
     describe 'GET #show' do
-      it "assigns the requested work to @work" do
+      it "redirects when... renders the :show template" do
         work = create(:work)
         get :show, id: work, client_id: @client
-        expect(assigns(:work)).to eq work
-      end
-      it "renders the :show template" do
-        work = create(:work)
-        get :show, id: work, client_id: @client
-        expect(response).to render_template :show
+        expect(response).to redirect_to root_url
       end
     end
 
     describe 'GET #new' do
-      it "assigns a new work to @work" do
+      it "does not… assign a new work to @work" do
         get :new, client_id: @client
-        expect(assigns(:work)).to be_a_new(Work)
+        expect(assigns(:work)).not_to be_a_new(Work)
       end
-      it "renders the :new template" do
+      it "redirects when... renders the :new template" do
         get :new, client_id: @client
-        expect(response).to render_template :new
+        expect(response).to redirect_to root_url
       end
       it "only shows current user's services"
     end
 
     describe 'GET #edit' do
-      it "assigns the requested work to @work" do
-        #work = create(:work)
+      it "redirects when... renders the :edit template" do
         get :edit, { client_id: @client.id, id: @work }
-        expect(assigns(:work)).to eq @work
-      end
-      it "renders the :edit template" do
-        get :edit, { client_id: @client.id, id: @work }
-        expect(response).to render_template :edit
+        expect(response).to redirect_to root_url
       end
     end
 
     describe 'POST #create' do
       context "with valid attributes" do
-      before :each do
-        @work_attributes = attributes_for(:work, client_id: @client)
-        @work_invalid = attributes_for(:invalid_work, client_id: @client)
-      end
-        it "saves the new work in the database" do
+        before :each do
+          @work_attributes = attributes_for(:work, client_id: @client)
+          @work_invalid = attributes_for(:invalid_work, client_id: @client)
+        end
+        it "does not... save the new work in the database" do
           expect{
             post :create, client_id: @client, work: @work_attributes
-          }.to change(Work, :count).by(1)
+          }.to change(Work, :count).by(0)
         end
-    ######
-      it "assigns the requested work to @work" do
-        work = create(:work)
-        get :show, id: work, client_id: @client
-        expect(assigns(:work)).to eq work
-      end
-      it "renders the :show template" do
-        work = create(:work)
-        get :show, id: work, client_id: @client
-        expect(response).to render_template :show
-      end
-    #####
-        it "redirects to works#show" do
+        it "does not... renders the :show template" do
+          work = create(:work)
+          get :show, id: work, client_id: @client
+          expect(response).to redirect_to root_url
+        end
+        it "does not... redirects to works#show" do
           @client_id = @client.id
           work = attributes_for(:work)
           post :create, 
             work: attributes_for(:work), client_id: @client
           #expect(response).to redirect_to(assigns(:work))
-          expect(response).to redirect_to(client_work_path(@work, @client_id))
-        end
-      end
-      context "with invalid attributes" do
-        it "does not save the new work in the database" do
-          expect{
-            post :create,
-            work: attributes_for(:invalid_work), client_id: @client
-          }.not_to change(Work, :count)
-        end
-        it "re-renders the :new template" do
-          post :create,
-            work: attributes_for(:invalid_work), client_id: @client
-          expect(response).to render_template :new
+          expect(response).to redirect_to root_url
         end
       end
     end
@@ -289,35 +260,30 @@ describe WorksController do
           client: @client)
       end
       context "with valid attributes" do
-        it "locates the requested @work" do
-          patch :update, id: @work, client_id: @client, 
-            work: attributes_for(:work, client_id: @client)
-          expect(assigns(:work)).to eq(@work)
-        end
-        it "changes @work's attributes" do
+        it "does not... change @work's attributes" do
           patch :update, client_id: @client, id: @work, 
             work: attributes_for(:work,
               title: 'The Bros, Inc.'), client_id: @client
           @work.reload
-          expect(@work.title).to eq('The Bros, Inc.')
+          expect(@work.title).to eq('Bro and Bro, Inc.')
         end
-        it "redirects to the updated work" do
+        it "does not... redirect to the updated work" do
           patch :update, client_id: @client, id: @work, work: attributes_for(:work)
-          expect(response).to redirect_to [@work.client, @work]
+          expect(response).to redirect_to root_url
         end
       end
       context "with invalid attributes" do
-        it "does not change the work's attributes" do
+        it "does not... change the work's attributes" do
           patch :update, client_id: @client, id: @work,
             work: attributes_for(:work,
               title: nil)
           @work.reload
           expect(@work.title).not_to eq(nil)
         end
-        it "re-renders the :edit template" do
+        it "does not... re-render the :edit template" do
           patch :update, id: @work, client_id: @client, 
             work: attributes_for(:invalid_work), client_id: @client
-          expect(response).to render_template :edit
+          expect(response).to redirect_to root_url
         end
       end
     end
@@ -325,14 +291,14 @@ describe WorksController do
     describe 'DELETE #destroy' do
       before :each do
       end
-      it "deletes the work" do
+      it "does not... delete the work" do
         expect{
           delete :destroy, { client_id: @client.id, id: @work }
-        }.to change(Work, :count).by(-1)
+        }.to change(Work, :count).by(0)
       end
-      it "redirects to users#index" do
+      it "does not... redirect to users#index" do
         delete :destroy, { client_id: @client.id, id: @work }
-        expect(response).to redirect_to client_works_url
+        expect(response).to redirect_to root_url
       end
     end
   end
